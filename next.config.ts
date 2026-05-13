1a9c46454fed6d772b86a25ecff309824ea494f0
KALIL-devs <kalilrahman055@gmail.com>
2026-05-13 09:28:59 +0530

    fix: next.config.ts duplicate output property

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
   images: { unoptimized: true },
   reactCompiler: true,
   output: "standalone",
 };
 
-export default nextConfig;
+export default nextConfig; 
